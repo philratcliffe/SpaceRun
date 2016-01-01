1cd3b5031d0089230dda8e7619ad1fdbc7ac4e48
Philip Ratcliffe <phil@philratcliffe.co.uk>
2016-01-01 15:24:26 +0000

    Added some debug statements.

--- Map.cpp
+++ Map.cpp
@@ -1,7 +1,9 @@
+#include <stdio.h>
 #include "libtcod.hpp"
 #include "Map.hpp"
 #include "Actor.hpp"
 #include "Engine.hpp"
+
 
 static const int ROOM_MAX_SIZE = 12;
 static const int ROOM_MIN_SIZE = 6;
@@ -16,6 +18,8 @@ public:
     BspListener(Map &map) : map(map), roomNum(0) {}
     bool visitNode(TCODBsp *node, void *userData) {
     	if ( node->isLeaf() ) {
+            printf ("\n\n*****  Start node for room %d *****\n", roomNum);
+            // This is a leaf, i.e., has no children / splits
     		int x,y,w,h;
 			// dig a room
 			TCODRandom *rng=TCODRandom::getInstance();
@@ -24,6 +28,7 @@ public:
 			x=rng->getInt(node->x+1, node->x+node->w-w-1);
 			y=rng->getInt(node->y+1, node->y+node->h-h-1);
 			map.createRoom(roomNum == 0, x, y, x+w-1, y+h-1);
+            printf("Middle of room: x=%d, y=%d\n", x+w/2, y+h/2);
 			if ( roomNum != 0 ) {
 			    // dig a corridor from last room
 			    map.dig(lastx,lasty,x+w/2,lasty);
@@ -31,6 +36,7 @@ public:
 			}
             lastx=x+w/2;
             lasty=y+h/2;
+            printf ("Finished node for Room %d\n\n", roomNum);
             roomNum++;
         }
         return true;
@@ -38,7 +44,9 @@ public:
 };
 
 Map::Map(int width, int height) : width(width),height(height) {
+    // We will make the tiles the size of one characater.
     tiles=new Tile[width*height];
+
     TCODBsp bsp(0,0,width,height);
     bsp.splitRecursive(NULL,8,ROOM_MAX_SIZE,ROOM_MAX_SIZE,1.5f,1.5f);
     BspListener listener(*this);
@@ -60,9 +68,19 @@ void Map::dig(int x1, int y1, int x2, int y2) {
         y2=y1;
         y1=tmp;
     }
+    int sumx = x2 - x1;
+    int sumy = y2 - y1;
+    printf("x2 - x1 %d\n", sumx);
+    printf("y2 - y1 %d\n", sumy);
     for (int tilex=x1; tilex <= x2; tilex++) {
         for (int tiley=y1; tiley <= y2; tiley++) {
-            tiles[tilex+tiley*width].canWalk=true;
+            // tiley * width gets number of tiles in complete horizontal rows 
+            // tilex adds the number of tiles on the current row
+            // e.g x is 2 y is 3
+            // ******
+            // ******
+            // **
+            tiles[tilex+(tiley*width)].canWalk=true;
         }
     }
 }
@@ -87,10 +105,10 @@ bool Map::isWall(int x, int y) const {
 }
  
 void Map::render() const {
-    static const TCODColor lightWall = TCODColor::white; 
+    //static const TCODColor lightWall = TCODColor::white; 
     static const TCODColor darkWall = TCODColor::lightGrey; 
     static const TCODColor darkGround = TCODColor::black; 
-    static const TCODColor lightGround = TCODColor::darkerGrey; 
+    //static const TCODColor lightGround = TCODColor::darkerGrey; 
 
 	for (int x=0; x < width; x++) {
 	    for (int y=0; y < height; y++) {
